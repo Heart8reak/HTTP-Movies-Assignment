f34aa04b503c76578d8bf09dd8d76e9e906da712
Carlos Baez <carlosbaez@Green-Planet.local>
2020-04-30 22:09:17 -0400

    intial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,13 @@
-import React, { useState } from "react";
-import { Route } from "react-router-dom";
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
-import UpdateMovie from './Movies/UpdateMovie'
-import AddMovie from './Movies/AddMovie'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-const App = () => {
-  const [savedList, setSavedList] = useState([]);
-
-  const addToSavedList = movie => {
-    setSavedList([...savedList, movie]);
-  };
-
+function App() {
   return (
-    <>
-      <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
-      <Route
-        path="/movies/:id"
-        render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
-        }}
-      />
-      <Route
-        path="/update-movie/:id"
-        render={props => {
-          return <UpdateMovie
-            {...props}
-            items={savedList}
-          />
-        }}
-      />
-      <Route
-        path="/add-movie/"
-        render={props => {
-          return <AddMovie
-            {...props}
-            items={savedList}
-          />
-        }}
-      />
-    </>
+    <div className="App">
+      We are trying something new here
+    </div>
   );
-};
+}
 
 export default App;
